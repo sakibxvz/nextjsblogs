abf2c08cc1bfe46413a258a292dfbac17380de22
sakibxvz <sakibxvz@gmail.com>
2024-05-29 19:33:34 +0600

    Prisma Set up done

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import BackButton from '@/components/BackButton';
 import FormPost from '@/components/FormPost';
 import { FormInputPost } from '@/types';
 import { SubmitHandler } from 'react-hook-form';
@@ -9,6 +10,7 @@ const CreatePage = () => {
 	};
 	return (
 		<div>
+			<BackButton/>
 			<h1 className='text-2xl my-4 font-bold text-center'>Add new post</h1>
 			<FormPost submit={handleCreatePost} isEditing={false} />
 		</div>
